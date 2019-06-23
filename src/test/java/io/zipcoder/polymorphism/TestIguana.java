b45b8a56351be45609f609a416811952262bd456
Erick Tepale <e.tepale@gmail.com>
2019-06-23 13:40:27 -0400

    forgot to add file

--- src/test/java/io/zipcoder/polymorphism/TestIguana.java
+++ src/test/java/io/zipcoder/polymorphism/TestIguana.java
@@ -1,4 +1,25 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestIguana {
+    @Test
+    public void testConstructor(){
+        //expected
+        Iguana a = new Iguana("Name");
+
+        //
+        Assert.assertFalse(a == null);
+    }
+
+    @Test
+    public void testSpeak(){
+        //Expected
+        String expected = "*stick tongue out twice*";
+
+        Iguana a = new Iguana("bleh");
+
+        Assert.assertEquals(expected, a.speak());
+    }
 }
